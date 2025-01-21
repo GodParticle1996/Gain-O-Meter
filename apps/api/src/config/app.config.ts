990d929f674412b634bc61016d97fc24e9d5f9dd
GodParticle1996 <vibekdutta3096@gmail.com>
2025-01-20 23:15:50 -0500

    DB connection issue fixed

--- apps/api/src/config/app.config.ts
+++ apps/api/src/config/app.config.ts
@@ -1,19 +1,23 @@
-import { getEnv } from "../common/utils/get-env";
+import { getEnv } from '../common/utils/get-env'
 
 const appConfig = () => ({
-  NODE_ENV: getEnv("NODE_ENV", "development"),
-  APP_ORIGIN: getEnv("APP_ORIGIN", "localhost"),
-  PORT: getEnv("PORT", "5000"),
-  BASE_PATH: getEnv("BASE_PATH", "/api/v1"),
-  MONGO_URI: getEnv("MONGO_URI"),
+  NODE_ENV: getEnv('NODE_ENV', 'development'),
+  APP_ORIGIN: getEnv('APP_ORIGIN', 'localhost'),
+  PORT: getEnv('PORT', '5000'),
+  BASE_PATH: getEnv('BASE_PATH', '/api/v1'),
+  MONGO_URI: getEnv(
+    'MONGO_URI',
+    'mongodb+srv://GodParticle1996:IqrCIm1Xozu7Nbmv@gainometercluster1.fdlmb.mongodb.net/?retryWrites=true&w=majority&appName=GainOMeterCluster1',
+  ),
+  MONGO_DB_NAME: getEnv('MONGO_DB_NAME'),
   JWT: {
-    SECRET: getEnv("JWT_SECRET"),
-    EXPIRES_IN: getEnv("JWT_EXPIRES_IN", "15m"),
-    REFRESH_SECRET: getEnv("JWT_REFRESH_SECRET"),
-    REFRESH_EXPIRES_IN: getEnv("JWT_REFRESH_EXPIRES_IN", "30d"),
+    SECRET: getEnv('JWT_SECRET'),
+    EXPIRES_IN: getEnv('JWT_EXPIRES_IN', '15m'),
+    REFRESH_SECRET: getEnv('JWT_REFRESH_SECRET'),
+    REFRESH_EXPIRES_IN: getEnv('JWT_REFRESH_EXPIRES_IN', '30d'),
   },
-  MAILER_SENDER: getEnv("MAILER_SENDER"),
-  RESEND_API_KEY: getEnv("RESEND_API_KEY"),
-});
+  MAILER_SENDER: getEnv('MAILER_SENDER'),
+  RESEND_API_KEY: getEnv('RESEND_API_KEY'),
+})
 
-export const config = appConfig();
+export const config = appConfig()
--- apps/api/src/database/database.ts
+++ apps/api/src/database/database.ts
@@ -1,14 +1,16 @@
-import mongoose from "mongoose";
-import { config } from "../config/app.config";
+import mongoose from 'mongoose'
+import { config } from '../config/app.config'
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(config.MONGO_URI);
-    console.log("Connected to Mongo database");
+    console.log('config.MONGO_URI', config.MONGO_URI)
+    await mongoose.connect(config.MONGO_URI)
+    console.log('Connected to Mongo database')
   } catch (error) {
-    console.log("Error connecting to Mongo database");
-    process.exit(1);
+    console.log('error', error)
+    console.log('Error connecting to Mongo database')
+    process.exit(1)
   }
-};
+}
 
-export default connectDatabase;
+export default connectDatabase
